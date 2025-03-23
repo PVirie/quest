420433e6219c23edf381d2c0667c497ee9042a5f
Chatavut Viriyasuthee <p.virie@gmail.com>
2025-03-23 19:09:26 +0700

    alfworld in graph format, cannot run yet due to missing some infos

--- tasks/rl_alfworld.py
+++ tasks/rl_alfworld.py
@@ -26,22 +26,73 @@ if len(os.listdir(alfworld_path)) == 0:
     # clone the Alfred data repository
     subprocess.run(["git", "clone", "https://github.com/alfworld/alfworld.git", f"{alfworld_path}/alfworld"])
 
-# load config
-with open(f"{alfworld_path}/alfworld/configs/base_config.yaml", 'r') as f:
-    config = yaml.safe_load(f)
-env_type = config['env']['type'] # 'AlfredTWEnv' or 'AlfredThorEnv' or 'AlfredHybrid'
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# setup environment
-env = get_environment(env_type)(config, train_eval='train')
-env = env.init_env(batch_size=1)
+from quest_interface import Quest_Graph, Action
+from implementations.rl_torch import agent_functions, rl_graph
+from implementations.rl_torch.persona import Persona
 
-# interact
-obs, info = env.reset()
-for i in range(5):
-    # get random actions from admissible 'valid' commands (not available for AlfredThorEnv)
-    admissible_commands = list(info['admissible_commands']) # note: BUTLER generates commands word-by-word without using admissible_commands
-    random_actions = [np.random.choice(admissible_commands[0])]
 
-    # step
-    obs, scores, dones, infos = env.step(random_actions)
-    print("Action: {}, Obs: {}".format(random_actions[0], obs[0]))
+MAX_VOCAB_SIZE = 1000
+tokenizer = utilities.Text_Tokenizer(MAX_VOCAB_SIZE, device)
+
+def play(env, agent, nb_episodes=10, verbose=True):
+    torch.manual_seed(20250301)  # For reproducibility when using action sampling.
+
+    persona = Persona(env, agent, tokenizer)
+    
+    # Collect some statistics: nb_steps, final reward.
+    avg_moves, avg_scores = [], []
+    for no_episode in range(nb_episodes):
+        obs, infos = env.reset()  # Start new episode.
+        score = 0
+        done = False
+        nb_moves = 0
+
+        root_node = rl_graph.Quest_Node(None, None)
+        working_memory = Quest_Graph(root_node)
+        working_memory.discover(rl_graph.Observation_Node(None, (obs, score, done, infos)), root_node)
+
+        while True:
+            action, param_1, param_2 = agent_functions.basic_tree(persona, working_memory.query())
+            if action == Action.ANSWER:
+                working_memory.respond(param_1, param_2)
+                if param_2 is None:
+                    if param_1 is None:
+                        break
+            elif action == Action.DISCOVER:
+                working_memory.discover(param_1, param_2)
+                if len(working_memory) > 100:
+                    break
+                nb_moves += 1
+            else:
+                raise ValueError("Invalid action")
+
+        if verbose:
+            print(".", end="")
+
+        last_observation = root_node.get_last_child().observation
+        score = last_observation[1]
+
+        avg_moves.append(nb_moves)
+        avg_scores.append(score)
+
+    if verbose:
+        msg = "  \tavg. steps: {:5.1f}; avg. score: {:4.1f} / {}."
+        print(msg.format(np.mean(avg_moves), np.mean(avg_scores), infos["max_score"]))
+
+
+if __name__ == "__main__":
+    # load config
+    with open(f"{alfworld_path}/alfworld/configs/base_config.yaml", 'r') as f:
+        config = yaml.safe_load(f)
+    env_type = config['env']['type'] # 'AlfredTWEnv' or 'AlfredThorEnv' or 'AlfredHybrid'
+
+    # setup environment
+    env = get_environment(env_type)(config, train_eval='train')
+    env = env.init_env(batch_size=1)
+
+    from implementations.example_tw_agents.agent_neural import RandomAgent, NeuralAgent
+    # agent = RandomAgent()
+    agent = NeuralAgent(input_size=MAX_VOCAB_SIZE, device=device)
+    play(env, agent, nb_episodes=100, verbose=True)
