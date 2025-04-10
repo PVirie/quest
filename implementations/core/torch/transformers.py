prepare code to fix fold computation.

implementations/core/torch/transformers.py
@@ -44,7 +44,7 @@ class Command_Scorer(nn.Module, Q_Table):
         self.action_decoder = nn.TransformerDecoder(decoder_layer, num_layers=1)
 
         decoder_layer = nn.TransformerDecoderLayer(d_model=hidden_size, nhead=8, device=device)
-        self.state_decoder = nn.TransformerDecoder(decoder_layer, num_layers=3)
+        self.state_decoder = nn.TransformerDecoder(decoder_layer, num_layers=4)
 
         self.critic = nn.Linear(hidden_size, 1, device=device)
         self.actor = nn.Linear(hidden_size, hidden_size, device=device)
@@ -73,11 +73,8 @@ class Command_Scorer(nn.Module, Q_Table):
         obs_embedding = apply_transformer(torch.reshape(obs_embedding, (-1, context_size, self.hidden_size)), self.context_decoder, tgt_mask=causal_mask(context_size, self.device), tgt_is_causal=True)
         obs_embedding = torch.reshape(obs_embedding[:, -1, :], (-1, n_contexts, self.hidden_size)) # batch x n_contexts x hidden
 
-        # accept (sequence_length, batch_size, d_model)
         obs_embedding = obs_embedding + self.pe[:n_contexts, :] # add positional encoding
-        obs_embedding = obs_embedding.permute(1, 0, 2) # n_contexts x batch x hidden
-        state_internal = self.state_decoder(obs_embedding, obs_embedding, tgt_mask=causal_mask(n_contexts, self.device), tgt_is_causal=True) # n_contexts x batch x hidden
-        state_internal = state_internal.permute(1, 0, 2) # batch x n_contexts x hidden
+        state_internal = apply_transformer(obs_embedding, self.state_decoder, tgt_mask=causal_mask(n_contexts, self.device), tgt_is_causal=True) # batch x n_contexts x hidden
         values = self.critic(state_internal)
 
         return values, state_internal

implementations/tw_agents/hierarchy_agent.py
@@ -55,15 +55,15 @@ class Hierarchy_Agent:
         # Get our next action and value prediction.
         action_scores, values = self.model(state_tensor, action_list_tensor)
 
+        action_scores = action_scores[0, -1, :]
+        values = values[0, -1, :].item()
+
         if sample_action:
-            probs = torch.nn.functional.softmax(action_scores[0, -1, ...], dim=0)  # n_actions
+            probs = torch.nn.functional.softmax(action_scores, dim=0)  # n_actions
             indices = torch.multinomial(probs, num_samples=1).item() # 1
         else:
             # greedy
-            indices = torch.argmax(action_scores[0, -1, :], dim=0).item()
-
-        action_scores = action_scores[0, -1, :]
-        values = values[0, -1, :].item()
+            indices = torch.argmax(action_scores, dim=0).item()
 
         return Value_Action(values, action_list[indices], self.iteration)
 
@@ -109,7 +109,7 @@ class Hierarchy_Agent:
         action_scores = torch.gather(action_scores[0, :, :], 0, torch.unsqueeze(context_marks, 1).expand(-1, action_scores.size(2)))
         values = torch.gather(values[0, :, 0], 0, context_marks)
 
-        action_indexes = torch.reshape(torch.tensor([action_list.index(va.selected_action) for _, va, _ in transitions], dtype=torch.int64, device=self.device), (-1, 1))
+        action_indexes = torch.reshape(torch.tensor([action_list.index(a) for _, a, _ in transitions], dtype=torch.int64, device=self.device), (-1, 1))
         rewards = torch.tensor([r for r, _, _ in transitions], dtype=torch.float32, device=self.device)
 
         returns, advantages = self._discount_rewards(last_value, values.flatten().detach(), rewards)
@@ -131,31 +131,26 @@ class Hierarchy_Agent:
 
         # use vector instead of loops
         probs = torch.nn.functional.softmax(action_scores, dim=1)
-        log_probs = torch.log(torch.clip(probs, min=1e-6))
+        log_probs = torch.log(probs)
         log_action_probs = torch.gather(log_probs, 1, action_indexes)
-        log_action_probs = log_action_probs.flatten()
+        log_action_probs = torch.clip(log_action_probs.flatten(), min=-8)
         policy_loss = (-log_action_probs * advantages).sum()
         value_loss = (.5 * (values - returns) ** 2.).sum()
         entropy = (-probs * log_probs).sum()
         loss = policy_loss + 0.5 * value_loss - 0.1 * entropy
 
+        self.ave_value = self.LOG_ALPHA * self.ave_value + (1 - self.LOG_ALPHA) * action_scores.mean().item()
 
-        if isinstance(loss, torch.Tensor):
-            self.ave_value = self.LOG_ALPHA * self.ave_value + (1 - self.LOG_ALPHA) * loss.item()
+        loss.backward()
+        nn.utils.clip_grad_norm_(self.model.parameters(), 40)
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+        self.iteration += 1
 
-            loss.backward()
-            nn.utils.clip_grad_norm_(self.model.parameters(), 40)
-            self.optimizer.step()
-            self.optimizer.zero_grad()
-            self.iteration += 1
-
-        for _, va, _ in transitions:
-            if va is not None and not va.has_released:
-                va.release()
 
 
     def print(self, step):
         msg = "{:6d}. ".format(step)
-        msg += "loss: {:5.2f}; ".format(self.ave_value)
+        msg += "value: {:5.2f}; ".format(self.ave_value)
         print(msg)
         